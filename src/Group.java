9a052723747b605409a2cfc22cba540101b5dfe2
yo hsiang chang <49606546+yohschang@users.noreply.github.com>
2021-06-13 23:29:11 +0800

    update

--- src/Group.java
+++ src/Group.java
@@ -2,7 +2,27 @@ import java.util.ArrayList;
 
 public class Group {
 
-    ArrayList<Person> fullPersonList = new ArrayList<Person>(5);
+    public final double inf = -9999999;
+
+    ArrayList<Person> fullPersonList = new ArrayList<Person>(); //store all person object
+    ArrayList<String> fullNameList = new ArrayList<String>(); // store all person name
+
+    public Group(){}
+
+    public Group(String personlist){
+        String[] namelist = personlist.split(",");
+        for(String name:namelist) {
+            fullPersonList.add(new Person(name));
+            fullNameList.add(name);
+        }
+    }
+
+    // for activity inheritance
+    public Group(Group group){
+        this.fullNameList = group.fullNameList;
+        this.fullPersonList = group.fullPersonList;
+    }
+
 
 
 }
--- src/Person.java
+++ src/Person.java
@@ -3,6 +3,10 @@ public class Person {
     int id  = 0;
     int credit = 0;
 
+    Person(String Name){
+        this.Name = Name;
+    }
+
     public void remind(String notification){
         System.out.println(notification);
     }
