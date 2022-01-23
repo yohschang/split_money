c26fd4b8c0b2bf41387bdf3c9a54705f14e7e3d1
r08945011 <r08945011@ntu.edu.tw>
2022-01-23 22:51:51 +0100

    update

--- split/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import SplitGroup, Activity, SplitUser
-
-# Register your models here.
-admin.site.register(SplitGroup)
-admin.site.register(Activity)
-admin.site.register(SplitUser)
